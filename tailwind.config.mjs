3995a4e763c0351d6cf515eb596982778bd4af21
Elvis Nipah <elvisnipah@gmail.com>
2024-11-01 17:34:04 +0000

    add header & image & colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      "white-smoke": "#EFF1ED",
+      flame: "#E4572E",
+      "raisin-black": "#2E282A",
+      "yellow-green": "#8ac926",
+      "steel-blue": "#1982C4",
+      "crayola-blue": "#3777ff",
+      "persian-rose": "#ff36ab",
+      "dark-cyan": "#0f8b8d",
+    },
+  },
+  plugins: [],
+};
